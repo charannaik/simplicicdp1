0d03b97cb4efb7a4b0f91310a629cb0157d51bf5
charannaik <charannaik@users.noreply.github.com>
2021-12-25 23:18:55 +0530

    Add files via upload

---
+++ src/main/java/springdemo/demoartifact1/DemoNameApplication.java
@@ -0,0 +1,13 @@
+package springdemo.demoartifact1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springdemo/demoartifact1/DemoNameApplicationTests.java
@@ -0,0 +1,13 @@
+package springdemo.demoartifact1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
